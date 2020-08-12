95be9b44ce1c2d965b60126a05c5806dc15354f6
tygemu <60887263+tygemu@users.noreply.github.com>
2020-08-12 22:18:17 +0800

    Update UnicomAutoGetCoin.py

--- UnicomAutoGetCoin.py
+++ UnicomAutoGetCoin.py
@@ -62,5 +62,5 @@ if __name__ == '__main__':
 
     timestamp = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
-    data = timestamp+"XXXXXXXXXXXX双引号中的内容换成抓包的结果reqtime=xxxx-xx-xx xx:xx:xx后面的内容XXXXXXXXXXX"
+    data = timestamp+"&simCount=1&version=iphone_c@7.0403&mobile=e1%2BPKn7E95gRQCPErqMts1dwdhOCtOpE422oa8wKXps7ZwdXXbE%2BIrUhNtWIAJoWtUYnDvXVnNSV3xgcbkR61bxTHMo6Vlm5%2BfOz531CAvppJeExhHmk3yEdOndR7VL1Sn2XrezLMHmxCqb8kQVCJ2bqJtrISRWJ%2BPcWTCDuH%2F8%3D&netWay=wifi&isRemberPwd=false&appId=af8ceac06275e6af2826a3cf6ea168d620b2ae3115fa1d53f024b2f4fc95d3fa5ceb3b0469fd7d4c95d5cae18e8ce669914a9973c879f1ddc0a1810e61e6f2c3a15ecda8dfee05df77c2450bcd609f2ad487820d385e47bc148742887a0c25dd&deviceId=dd3ce2aba74aa1c7f9ff944b8701bd64b92508a0a2ab0ac501a3752d13121af2&pip=192.168.0.102&password=hwZYb%2BQZLIRnWvkR3UZuEJtNVemBXZ6rWWNEsbcDgRq6UT7%2FtXUxk67M5PmQ5cJNl7KQngqdHnIC%2FFVrHiUbLzbezErNFNevXwuhMXjR%2F1%2BFZF9OmOo%2BpYlf9KJADi1VW4KUp3QbKFnZuRGYhCua2Jj2e1AhhmAmvdbyORMyKgI%3D&deviceOS=13.6&deviceBrand=iphone&deviceModel=iPhone&remark4=&keyVersion=&deviceCode=2D55D64A-28B6-48C8-BF6B-0B0980136C01"
     user.sign(data)
